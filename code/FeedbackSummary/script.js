ba78289dfaea68cae73a50ff497a658b4c2805da
Elias Lai <eliaslylai@gmail.com>
2023-06-17 22:03:01 -0500

    fixed timpestamp jumping feature

--- code/FeedbackSummary/script.js
+++ code/FeedbackSummary/script.js
@@ -1,145 +1,144 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('videoForm');
-    const teamIdInput = document.getElementById('teamId');
-    const takeNumberInput = document.getElementById('takeNumber');
-    const videoPlayer = document.getElementById('videoPlayer');
-    const educatorTable = document.getElementById('educatorTable');
+  const form = document.getElementById('videoForm');
+  const teamIdInput = document.getElementById('teamId');
+  const takeNumberInput = document.getElementById('takeNumber');
+  const videoPlayer = document.getElementById('videoPlayer');
+  const educatorTable = document.getElementById('educatorTable');
 
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
+  form.addEventListener('submit', function(e) {
+      e.preventDefault();
 
-        const teamId = Number(teamIdInput.value);
-        const takeNumber = Number(takeNumberInput.value);
-        const videoPath = `../Data/Videos/TID${teamId}TN${takeNumber}.mp4`;
+      const teamId = Number(teamIdInput.value);
+      const takeNumber = Number(takeNumberInput.value);
+      const videoPath = `../Data/Videos/TID${teamId}TN${takeNumber}.mp4`;
 
-        videoPlayer.innerHTML = `
-            <video controls>
-                <source src="${videoPath}" type="video/mp4">
-            </video>
-        `;
+      videoPlayer.innerHTML = `
+          <video controls>
+              <source src="${videoPath}" type="video/mp4">
+          </video>
+      `;
 
-        loadEducatorData(teamId, takeNumber);
-    });
+      loadEducatorData(teamId, takeNumber);
+  });
 
-    function loadEducatorData(teamId, takeNumber) {
-        const csvPath = '../Data/educator_data.csv';
+  function loadEducatorData(teamId, takeNumber) {
+    const csvPath = '../Data/educator_data.csv';
 
-        fetch(csvPath)
-            .then(response => response.text())
-            .then(csvData => {
-                const rows = csvData.split('\n');
-                const tableHeaders = rows[0].split(',');
-                const tableBody = document.createElement('tbody');
+    fetch(csvPath)
+        .then(response => response.text())
+        .then(csvData => {
+            const rows = csvData.split('\n');
+            const tableHeaders = rows[0].split(',');
+            const tableBody = document.createElement('tbody');
 
-                // Add reflection column header
-                tableHeaders.push('Reflection');
+            // Add reflection column header
+            tableHeaders.push('Reflection');
 
-                // Create table header row
-                const tableHeaderRow = document.createElement('tr');
-                tableHeaders.forEach(header => {
-                    const th = document.createElement('th');
-                    th.textContent = header;
-                    tableHeaderRow.appendChild(th);
-                });
+            // Create table header row
+            const tableHeaderRow = document.createElement('tr');
+            tableHeaders.forEach(header => {
+                const th = document.createElement('th');
+                th.textContent = header;
+                tableHeaderRow.appendChild(th);
+            });
 
-                tableBody.appendChild(tableHeaderRow);
+            tableBody.appendChild(tableHeaderRow);
 
-                // Create table rows
-                for (let i = 1; i < rows.length; i++) {
-                    const row = rows[i].split(',');
-                    const rowData = row.slice(0, 6); // Get the first 6 columns from the CSV
+            // Create table rows
+            for (let i = 1; i < rows.length; i++) {
+                const row = rows[i].split(',');
+                const rowData = row.slice(0, 6); // Get the first 6 columns from the CSV
 
-                    if (rowData[4] == teamId && rowData[5] == takeNumber) {
-                        const tableRow = document.createElement('tr');
+                if (rowData[4] == teamId && rowData[5] == takeNumber) {
+                    const tableRow = document.createElement('tr');
 
-                        // Check color value and replace with color rectangle
-                        const colorValue = Number(rowData[3]);
-                        let colorClass = '';
-                        if (colorValue === 1) {
-                            colorClass = 'red';
-                        } else if (colorValue === 3) {
-                            colorClass = 'yellow';
-                        } else if (colorValue === 5) {
-                            colorClass = 'green';
-                        }
+                    // Add reflection column with input field
+                    rowData.push(`<input class="reflection-input" type="text" value="">`);
 
-                        rowData[3] = `<div class="color-rectangle ${colorClass}"></div>`;
-
-                        // Add reflection column with input field
-                        rowData.push(`<input class="reflection-input" type="text" value="">`);
-
-                        rowData.forEach(data => {
-                            const td = document.createElement('td');
+                    rowData.forEach((data, index) => {
+                        const td = document.createElement('td');
+                        if (index === 3) {
+                          // Create hyperlink with timestamp
+                            const timestamp = rowData[3];
+                            const hyperlink = document.createElement('a');
+                            hyperlink.href = '#'; // Replace '#' with the appropriate video timestamp
+                            hyperlink.textContent = timestamp;
+                            hyperlink.addEventListener('click', function() {
+                              jumpToTimestamp(rowData[3]);
+                          });
+                            td.appendChild(hyperlink);
+                        } else {
                             td.innerHTML = data;
-                            tableRow.appendChild(td);
-                        });
+                        }
+                        tableRow.appendChild(td);
+                    });
 
-                        tableBody.appendChild(tableRow);
-                    }
+                    tableBody.appendChild(tableRow);
                 }
+            }
 
-                educatorTable.innerHTML = '';
-                educatorTable.appendChild(tableBody);
-            })
-            .catch(error => console.error('Error:', error));
-    }
+            educatorTable.innerHTML = '';
+            educatorTable.appendChild(tableBody);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function jumpToTimestamp(timestamp) {
+  // Extract hours, minutes, and seconds from the timestamp
+  var [hours, minutes, seconds] = timestamp.split(':');
+
+  // Calculate the total number of seconds
+  var totalSeconds = parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
+
+  // Seek to the specified timestamp in the video
+  var video = document.querySelector('#videoPlayer video');
+  video.currentTime = totalSeconds;
+}
 
 // Function to send data to the server and append to the CSV file
 function appendToCSV(data) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "append_csv.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText); // Output the server response
-        showMessage("Reflections saved");
-        setTimeout(function() {
-          window.location.href = "../index.html";
-        }, 2000); // Redirect after 2 seconds
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "append_csv.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText); // Output the server response
+    }
+  };
+  xhr.send("data=" + encodeURIComponent(data));
+}
+
+// Function to export the table data as a CSV and append to the server-side file
+function handleExportButtonClick() {
+  var csv = [];
+  var rows = document.querySelectorAll("#educatorTable tr");
+
+  for (var i = 0; i < rows.length; i++) {
+    var row = [], cols = rows[i].querySelectorAll("td");
+
+    for (var j = 0; j < cols.length - 1; j++) {
+      row.push(cols[j].innerText);
+    }
+
+    // Get the reflection input value if it exists
+    if (cols.length > 6) {
+      var reflectionInput = cols[6].querySelector("input");
+      if (reflectionInput) {
+        row.push(reflectionInput.value);
       }
-    };
-    xhr.send("data=" + encodeURIComponent(data));
+    }
+
+    csv.push(row.join(","));
   }
 
-  // Function to show a message on the page
-  function showMessage(message) {
-    var messageContainer = document.createElement("div");
-    messageContainer.className = "message";
-    messageContainer.textContent = message;
-    document.body.appendChild(messageContainer);
-  }
-  
-  // Function to export the table data as a CSV and append to the server-side file
-  function handleExportButtonClick() {
-    var csv = [];
-    var rows = document.querySelectorAll("#educatorTable tr");
-  
-    for (var i = 0; i < rows.length; i++) {
-      var row = [], cols = rows[i].querySelectorAll("td");
-  
-      for (var j = 0; j < cols.length - 1; j++) {
-        row.push(cols[j].innerText);
-      }
-  
-      // Get the reflection input value if it exists
-      if (cols.length > 6) {
-        var reflectionInput = cols[6].querySelector("input");
-        if (reflectionInput) {
-          row.push(reflectionInput.value);
-        }
-      }
-  
-      csv.push(row.join(","));
-    }
-  
-    var csvData = csv.join("\n");
-    appendToCSV(csvData);
-  }
-  
-  // Attach event listener to the export button
-  var exportButton = document.getElementById("exportButton");
-  exportButton.addEventListener("click", handleExportButtonClick);
-  
+  var csvData = csv.join("\n");
+  appendToCSV(csvData);
+}
+
+// Attach event listener to the export button
+var exportButton = document.getElementById("exportButton");
+exportButton.addEventListener("click", handleExportButtonClick);
+
 
 });
 
